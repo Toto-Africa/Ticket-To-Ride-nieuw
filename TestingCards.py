8a317e70c713178bae6d15ec3a374a85431a78a7
Elmer <elmerswaenen@gmail.com>
2018-12-06 15:35:37 +0100

    Commit update GUI

--- TestingCards.py
+++ TestingCards.py
@@ -10,16 +10,19 @@ class Test(object):
         deck = TrainCards.TrainCards()
         missions = MissionCards.MissionCards()
 
-        for i in range(80):
-                time.sleep(0.1)
-                ticket = deck.dealCard()
-                if ticket == 'gedaan':
-                        print("Einde Spel")
-                        break
-                else:
-                        print(ticket)
+        #for i in range(80):
+        #        time.sleep(0.1)
+        #        ticket = deck.dealCard()
+        #        if ticket == 'gedaan':
+        #                print("Einde Spel")
+        #                break
+        #        else:
+        #                print(ticket)
         #print(ticket[0])
-        #print(ticket[1][0])    --> GEEFT EERSTE MOGELIJKE ROUTE VAN TICKET
+        ticket = missions.dealMission()
+        print(ticket[0])    #--> GEEFT EERSTE MOGELIJKE ROUTE VAN TICKET
+        print(len(ticket[1]))
+        print(ticket[1][1])
         #lijst = list(ticket)
         # print(lijst)
         #ticket = deck.dealCard()
