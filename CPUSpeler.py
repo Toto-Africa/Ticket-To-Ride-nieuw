8b518cfa2a35b9743710b87fde9e7e6fb7135de8
Jan <vangompel.jan@hotmail.Com>
2018-12-12 15:25:37 +0100

    Route toegevoegd door Jakobus

--- CPUSpeler.py
+++ CPUSpeler.py
@@ -35,3 +35,26 @@ class CPUSpeler(Speler.Speler):
 
     def is_cpu(self):
         return True
+
+    def remove_cards_from_hand(self, color, amount):
+        aantalMetWilds = self.hand[color]+self.hand["wild"]
+        aantalPionnen = self.get_pawns()
+        bewerkingCode = 0 # 0 = geldig, 1 = ongeldig. Te weinig resources, 2 = spel afgelopen
+        if(aantalMetWilds < amount or aantalPionnen < amount):
+            bewerkingCode = 1
+        else:
+            if(self.hand[color]<amount):
+                rest = amount - self.hand[color]
+                self.hand[color] = 0
+                self.hand["wild"] = self.hand["wild"] - rest
+                self.remove_pawns(amount)
+                if self.get_pawns() == 0:
+                    bewerkingCode = 2
+                    #messagebox.showwarning("Spel afgelopen", "Speler: " + self.__name + " heeft geen pionnen meer over!")
+            else:
+                self.hand[color] = self.hand[color] - amount
+                self.remove_pawns(amount)
+                if self.get_pawns() == 0:
+                    bewerkingCode = 2
+                    #messagebox.showwarning("Spel afgelopen", "Speler: " + self.__name + " heeft geen pionnen meer over!")
+        return bewerkingCode
