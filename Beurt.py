d3d23ac58686d074c30f5d2064fa64a54e568117
Test <kkk@kkk.kkk>
2018-12-13 15:25:26 +0100

    Methode om controle bij swap_mission te implementeren

--- Beurt.py
+++ Beurt.py
@@ -95,13 +95,19 @@ class Beurt:
             return cpu_z
 
     # def swap_mission(self, pl = Speler.Speler, mission_to_change = str()): # Correct????
-    def swap_mission(self, pl, mission_to_change): # Correct????
+    def swap_mission(self, pl = Speler.Speler): # Correct????
         # TODO Check bij wisselen van missiekaarten
         # Iedere mogelijke route/weg afgaan en kijken of daar al 1 van ingenomen is
         # Als van alle mogelijke routes/wegen (tussen de 2 missiesteden) minstens eentje is ingenomen, dan is het onmogelijk
-        new_mission1 = self.missioncards.dealMission()
-        new_mission2 = self.missioncards.dealMission()
-        pl.set_missions(new_mission1, new_mission2)
+
+        bool = False
+
+        # Controle of aan voorwaarden voldaan is
+
+        if bool:
+            new_mission1 = self.missioncards.dealMission()
+            new_mission2 = self.missioncards.dealMission()
+            pl.set_missions(new_mission1, new_mission2)
 
 
     #def extra_traincard(self, pl = Speler.Speler):
